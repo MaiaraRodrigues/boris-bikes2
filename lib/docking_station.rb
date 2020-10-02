24d8b5a83a0edeb0ab47ffee16bd19666db9444e
Maiara <maiara@Maiaras-MacBook-Air.local>
2020-10-02 16:52:29 +0100

    step 12

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,20 @@
 require_relative 'bike'
 
 class DockingStation
+  
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
 
- def release_bike
-    Bike.new
- end
+  def release_bike
+    fail 'No bikes available' if @bikes.empty?
+    @bikes.pop
+  end
 
- def dock(bike)
-   bike 
- end
+  def dock(bike)
+    fail 'Docking station full' if @bikes.count >= 20
+    @bikes << bike
+  end
 
- def bike
-
- end 
 end
