758b22d26bdfe667b54d0fe610c60a4e42278b87
Maiara <maiara@Maiaras-MacBook-Air.local>
2020-10-02 20:14:01 +0100

    step 18, first user story

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,10 @@
 require 'Bike'
-describe Bike do
+ describe Bike do
     it { is_expected.to respond_to :working? } 
-end 
+ end 
+ describe Bike do
+    it 'can be reported broken' do
+      subject.report_broken
+      expect(subject).to be_broken
+    end
+end   
